f24fa5168880d022f4c03cf91a359dd30c30dce2
Andy Casey <andycasey@gmail.com>
2017-06-02 00:55:52 +1000

    bbbbg

--- .ci/travis.sh
+++ .ci/travis.sh
@@ -1,11 +1,7 @@
 #!/bin/bash -x
 
-# Build the paper(s)
-cd $TRAVIS_BUILD_DIR/articles/chemical-tagging-gmm
-make
-#cd $TRAVIS_BUILD_DIR/articles/nips-2017
-#make
-cd $TRAVIS_BUILD_DIR/articles/gmm-search
+# Build documents
+cd $TRAVIS_BUILD_DIR/document
 make
 
 # Push to GitHub
@@ -13,7 +9,7 @@ if [ -n "$GITHUB_API_KEY" ]; then
   cd $TRAVIS_BUILD_DIR
   git checkout --orphan $TRAVIS_BRANCH-pdf
   git rm -rf .
-  git add -f articles/*/ms.pdf
+  git add -f document/*.pdf
   git -c user.name='travis' -c user.email='travis' commit -m "Compiling PDFs"
   git push -q -f https://$GITHUB_USER:$GITHUB_API_KEY@github.com/$TRAVIS_REPO_SLUG $TRAVIS_BRANCH-pdf
 fi
